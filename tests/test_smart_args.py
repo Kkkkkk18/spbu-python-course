eec2af40656e3daf2b97068c326dffa04e3f5cb6
Varya Medvedeva <barbara.med56@mail.ru>
2024-10-16 04:39:51 +0300

    fix mistakes, add tests for smart_args, add documentation

--- tests/test_smart_args.py
+++ tests/test_smart_args.py
@@ -8,23 +8,99 @@ from project.smart_args import (
 
 
 def test_evaluated():
+
+    """
+    Test the Evaluated class functionality.
+
+    """
+
     def get_random_number():
+
+        """
+        Returns a random integer between 1 and 10.
+
+        """
+
         return random.randint(1, 10)
 
+    @smart_args
     def evaluation(*, x=get_random_number(), y=Evaluated(get_random_number)):
         return x, y
 
     res_1 = evaluation()
     res_2 = evaluation()
-
+    res_3 = evaluation(3)
     assert res_1[0] == res_2[0]
-    assert res_1[1] != res_2[1]
+    assert res_3 == 3
 
 
 def test_isolated():
+
+    """
+    Test the Isolated class functionality.
+
+    """
+
+    @smart_args
     def isolation(*, d=Isolated()):
         d["a"] = 0
         return d
 
-    res = isolation(d={"a": 10})
+    no_mutable = {"a": 10}
+    res = isolation(d=no_mutable)
+    assert no_mutable == {"a": 10}
     assert res == {"a": 0}
+
+
+def test_evaluated_isolated_combination():
+
+    """
+    Test the combination of Evaluated and Isolated classes.
+
+    """
+
+    @smart_args
+    def f(*, x, y=Evaluated(Isolated)):
+        return x + y
+
+    with pytest.raises(ValueError):
+        f(x=52)
+    with pytest.raises(ValueError):
+        f(x=1, y=2)
+
+
+def test_isolated_error():
+
+    """
+    Test the error handling of the Isolated class.
+
+    """
+
+    @smart_args
+    def isolation(*, d=Isolated()):
+        d["a"] = 0
+        return d
+
+    with pytest.raises(ValueError):
+        isolation()
+
+
+def test_evaluated_isolated():
+
+    """
+    Test the error handling of the Isolated class.
+
+    """
+
+    def get_random_number():
+        return random.randint(1, 10)
+
+    @smart_args
+    def evaluated_isolated(*, x=Isolated(), y=Evaluated(get_random_number)):
+        x["a"] = 0
+        return (x, y)
+
+    no_mutable = {"a": 10}
+    res = evaluated_isolated(x=no_mutable)
+    assert res == ({"a": 0}, 1)
+    assert no_mutable == {"a": 10}
